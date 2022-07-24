a708565aefd18721da4ee086304ec797ffaafb82
Brian H. Hough | brianhuff.eth <56947075+BrianHHough@users.noreply.github.com>
2022-07-24 11:20:26 -0400

    ✅ SVG base64 encoder via Solidity

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,23 +2,84 @@
 
 pragma solidity ^0.8.10;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // OpenZeppelin NFT contract w/ storage
+import "@openzeppelin/contracts/utils/Counters.sol"; // OpenZeppelin counters utility
+
+import { StringUtils } from "./libraries/StringUtils.sol"; // String utility
+import { Base64 } from "./libraries/Base64.sol"; // Convert to SVG
 import "hardhat/console.sol";
 
-contract ScoopsDomains {
-    mapping(string => address) public domains; // Mapping for owner's name::address
+contract ScoopsDomains is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
+    string public scoopdomain; // Create domain `scoopdomain`
+
+    string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#a)" d="M0 0h270v270H0z"/><defs><filter id="d" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><svg width="98" height="157" viewBox="0 0 98 137" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="url(#b)" d="M0 0h98v137H0z"/><defs><pattern id="b" patternContentUnits="objectBoundingBox" width="1" height="1"><use xlink:href="#c" transform="matrix(.00309 0 0 .0022 -.053 -.001)"/></pattern><image id="c" width="400" height="400" xlink:href="https://gateway.pinata.cloud/ipfs/QmSQ7RdEQRzr7FqLcV6pLf7S4arU2kkjhpQyzSSf1tUC5X"/></defs></svg><defs><linearGradient id="a" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#FBB336"/><stop offset="1" stop-color="#FA0955" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#d)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif">';
+    string svgPartTwo = '</text></svg>';
+
+    mapping(string => address) public domains; // Mapping for owner's name::address
     mapping(string => string) public records; // Mapping for storing values
 
     // Constructor to initiate contract
-    constructor() {
-        console.log("GM...THIS IS MY DOMAINS CONTRACT! WAGMI");
+    constructor(string memory _scoopdomain) payable ERC721("Scoops Domains on Polygon", "SDP") { // Contract is payable via constructor
+        scoopdomain = _scoopdomain; // Set values equal to themselves for domain resolver
+        console.log("%s Scoops Domain deployed,", _scoopdomain);
+    }
+
+    // Get the price of a domain using length of the string
+    function priceOfDomain(string calldata name) public pure returns(uint) {
+        uint lengthOfDomain = StringUtils.stringLength(name);
+        require(lengthOfDomain > 0);
+        if (lengthOfDomain == 3) { // 1 MATIC = 1 000 000 000 000 000 000 GWEI (18 decimal points)
+            return 5 * 10**16; // 0.05 MATIC
+        } else if (lengthOfDomain == 4) {
+            return 3 * 10**16; // 0.03 MATIC
+        } else {
+            return 1 * 10**16; // 0.01 MATIC
+        }
     }
 
     // Let owner add name to mapping
-    function registerDomainToOwner(string calldata name) public {
+    function registerDomainToOwner(string calldata name) public payable {
         require(domains[name] == address(0)); // Require: ensure name not registered yet
+        
+        uint256 _priceOfDomain = priceOfDomain(name); // Set the _priceOfDomain = that of the name that's set
+        require(msg.value >= _priceOfDomain, "Insufficient MATIC for Scoops Domain purchase"); // Ensure enough MATIC put into contract via `priceOfDomain`
+        
+        string memory _name = string(abi.encodePacked(name, ".", scoopdomain));
+
+        string memory finalSvg = string(abi.encodePacked(svgPartOne, _name, svgPartTwo));
+        uint256 newRecordId = _tokenIds.current();
+        uint256 length = StringUtils.stringLength(name);
+        string memory stringLength = Strings.toString(length);
+
+        console.log("Registering %s.%s on the contract with tokenID %d", name, scoopdomain, newRecordId);
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                _name,
+                '", "description": "A freshly minted Scoops Domains on Polygon", "image": "data:image/svg+xml;base64,',
+                Base64.encode(bytes(finalSvg)),
+                '","length":"',
+                stringLength,
+                '"}'
+            )
+        );
+
+        string memory finalTokenUri = string(abi.encodePacked("data:application/json;base64,", json));
+
+        console.log("\n--------------------------------------------------------");
+        console.log("Final tokenURI:", finalTokenUri);
+        console.log("--------------------------------------------------------\n");
+
+        _safeMint(msg.sender, newRecordId);
+        _setTokenURI(newRecordId, finalTokenUri);
         domains[name] = msg.sender; // Link domain name w/ the minter (message.sender)
-        console.log("%s has minted a scoops domain!", msg.sender);
+        
+        _tokenIds.increment();
+        // console.log("%s has minted a Scoops Domain!", msg.sender);
     }
 
     // Return domain owner's wallet address
@@ -29,7 +90,7 @@ contract ScoopsDomains {
     // Verify and set the owner to their domain
     function setRecordOfDomain(string calldata name, string calldata record) public {
         require(domains[name] == msg.sender); // Require: confirm domain owner is txn sender
-        records[name] = record;
+        records[name] = record; // Set the name record to the record
     }
 
     // Get the domain's record
--- scripts/run.js
+++ scripts/run.js
@@ -2,18 +2,28 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners(); // Get address of contract owner + random one
     const domainContractFactory = await hre.ethers.getContractFactory('ScoopsDomains'); // compiles contract 'Domains.sol'
-    const domainContract = await domainContractFactory.deploy(); // deploy contract to blockchain
+    const domainContract = await domainContractFactory.deploy("poly"); // deploy contract to blockchain and append .poly
     
     await domainContract.deployed(); // wait until contract mined/deployed
     
-    console.log("Domains contract deployed to: ", domainContract.address); // give address where deployed contract is at
+    console.log("===================================================================================");
+    console.log("Scoops Domains contract deployed to: ", domainContract.address); // give address where deployed contract is at
+    
+    console.log("===================================================================================");
     console.log("Contract deployed by: ", owner.address); // give address of person deploying contract
 
-    let txn = await domainContract.registerDomainToOwner("polygon"); // Simulate register a name and call address
+    let txn = await domainContract.registerDomainToOwner("polygon", {value: hre.ethers.utils.parseEther('0.01')}); // Simulate register a name and call address
     await txn.wait();
 
-    const domainOwner = await domainContract.getOwnerAddress("polygon"); // Simulate getting address of domain `polygon`
-    console.log("Owner of domain `polygon` is: ", domainOwner);
+    const domainOwnerAddress = await domainContract.getOwnerAddress("polygon"); // Simulate getting address of domain `polygon`
+    
+    console.log("===================================================================================");
+    console.log("Owner of Scoops Domain `polygon` is: ", domainOwnerAddress);
+
+    const balanceOfContract = await hre.ethers.provider.getBalance(domainContract.address);
+
+    console.log("===================================================================================");
+    console.log("Balance of contract:", hre.ethers.utils.formatEther(balanceOfContract));
 
     // Error Testing: set a record that doesn't belong to owner
     // txn = await domainContract.connect(randomPerson).setRecordOfDomain("polygon", "This is my scoops domain now (but it shouldn't be)");
