1d15d22df5a3eef23318ccda61a361f7be7cbde9
Brian H. Hough | brianhuff.eth <56947075+BrianHHough@users.noreply.github.com>
2022-07-24 01:09:49 -0400

    Mappings for addresses + domain names

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,22 +5,36 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract ScoopsDomains {
-    // Mapping for owner's name::address
-    mapping(string => address) public domains;
+    mapping(string => address) public domains; // Mapping for owner's name::address
 
+    mapping(string => string) public records; // Mapping for storing values
+
+    // Constructor to initiate contract
     constructor() {
         console.log("GM...THIS IS MY DOMAINS CONTRACT! WAGMI");
     }
 
     // Let owner add name to mapping
     function registerDomainToOwner(string calldata name) public {
-        domains[name] = msg.sender;
+        require(domains[name] == address(0)); // Require: ensure name not registered yet
+        domains[name] = msg.sender; // Link domain name w/ the minter (message.sender)
         console.log("%s has minted a scoops domain!", msg.sender);
     }
 
     // Return domain owner's wallet address
     function getOwnerAddress(string calldata name) public view returns (address) {
-        return domains[name];
+        return domains[name]; // Confirm owner is the domain txn sender
+    }
+
+    // Verify and set the owner to their domain
+    function setRecordOfDomain(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender); // Require: confirm domain owner is txn sender
+        records[name] = record;
+    }
+
+    // Get the domain's record
+    function getRecordOfDomain(string calldata name) public view returns(string memory) {
+        return records[name]; // Retrieve domain's record by name
     }
 }
 
--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,23 @@
-const { hexStripZeros } = require("ethers/lib/utils")
 
 const main = async () => {
-    // compiles contract 'Domains.sol'
-    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    // deploy contract to blockchain
-    const domainContract = await domainContractFactory.deploy();
-    // wait until contract mined/deployed
-    await domainContract.deployed();
-    // give addr of deployed contract
-    console.log("Domains contract deployed to: ", domainContract.address);
+    const [owner, randomPerson] = await hre.ethers.getSigners(); // Get address of contract owner + random one
+    const domainContractFactory = await hre.ethers.getContractFactory('ScoopsDomains'); // compiles contract 'Domains.sol'
+    const domainContract = await domainContractFactory.deploy(); // deploy contract to blockchain
+    
+    await domainContract.deployed(); // wait until contract mined/deployed
+    
+    console.log("Domains contract deployed to: ", domainContract.address); // give address where deployed contract is at
+    console.log("Contract deployed by: ", owner.address); // give address of person deploying contract
+
+    let txn = await domainContract.registerDomainToOwner("polygon"); // Simulate register a name and call address
+    await txn.wait();
+
+    const domainOwner = await domainContract.getOwnerAddress("polygon"); // Simulate getting address of domain `polygon`
+    console.log("Owner of domain `polygon` is: ", domainOwner);
+
+    // Error Testing: set a record that doesn't belong to owner
+    // txn = await domainContract.connect(randomPerson).setRecordOfDomain("polygon", "This is my scoops domain now (but it shouldn't be)");
+    // await txn.wait();
 };
 
 const runMain = async () => {
@@ -17,8 +26,8 @@ const runMain = async () => {
         process.exit(0);
     } catch (error) {
         console.log(error);
-        process.exit(1)
+        process.exit(1);
     }
-}
+};
 
 runMain();
